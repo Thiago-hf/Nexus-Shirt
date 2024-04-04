8ddd6d2c6823963d1c3a991aeaed91ac860511a4
Thiago Henrique Figueredo de Oliveira <164034177+Thiago-hf@users.noreply.github.com>
2024-04-03 22:37:04 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function basic() {
+    window.location.href = "https://api.whatsapp.com/send?phone=5531998836687&text=Ol%C3%A1,%20acabei%20de%20escolher%20o%20plano%20Basic,%20poderia%20me%20dar%20mais%20informa%C3%A7%C3%B5es?"
+}
+
+function fashion() {
+    window.location.href = "https://api.whatsapp.com/send?phone=5531998836687&text=Ol%C3%A1,%20acabei%20de%20escolher%20o%20plano%20Fashion,%20poderia%20me%20dar%20mais%20informa%C3%A7%C3%B5es?"
+}
+
+function icon() {
+    window.location.href = "https://api.whatsapp.com/send?phone=5531998836687&text=Ol%C3%A1,%20acabei%20de%20escolher%20o%20plano%20Icon,%20poderia%20me%20dar%20mais%20informa%C3%A7%C3%B5es?"
+}
